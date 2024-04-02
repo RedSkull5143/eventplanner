4c0e160dfe2ff06bbfa26b5710cd524f7c0c6363
RedSkull <omshinde5143@gmail.com>
2024-04-02 16:22:29 +0530

    first test case

--- src/main/java/org/omshinde/Main.java
+++ src/main/java/org/omshinde/Main.java
@@ -1,7 +1,13 @@
 package org.omshinde;
 
+import org.omshinde.models.Event;
+import org.omshinde.services.EventPlanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EventPlanner eventPlanner = new EventPlanner();
+        eventPlanner.addEvent(new Event("Automation Ultralesson", "2024-03-24", "QK Office"));
+        eventPlanner.addEvent(new Event("Saurav Mundan", "2024-04-02", "Amravati"));
+        eventPlanner.displayEvents();
     }
 }
